3cb4f13706458db51811bd42fbee05dccd3bd52b
Autumn Raine <autumn@inerdgirl.com>
2022-06-07 17:37:35 -0600

    Initial Commit

---
+++ Shared/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Shared
+//
+//  Created by Raven Hamilton-Raine on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
