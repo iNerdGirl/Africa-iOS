198653b8492e9993e0a247e5c7d5779afa970699
Autumn Raine <autumn@inerdgirl.com>
2022-06-08 14:30:15 -0600

    Added VideoListItemView

--- Shared/App/VideoListView.swift
+++ Shared/App/VideoListView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct VideoListView: View {
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    
     var body: some View {
-        Text("VideoListView")
+        NavigationView {
+            List {
+                ForEach(videos) { video in
+                    VideoListItemView(video: video)
+                        .padding(.vertical, 8)
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        videos.shuffle()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        }
     }
 }
 
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
+        .previewDevice("iPhone 13 Pro")
     }
 }
