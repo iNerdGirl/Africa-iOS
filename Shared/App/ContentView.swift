452ad563854aeceea5eabed4500b743da960d635
Autumn Raine <autumn@inerdgirl.com>
2022-06-07 20:13:23 -0600

    Added CoverImageView to ContentView

--- Shared/App/ContentView.swift
+++ Shared/App/ContentView.swift
@@ -9,12 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("ContentView")
+        NavigationView {
+            List {
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            }
+            .navigationBarTitle("Africa", displayMode: .large)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro")
     }
 }
